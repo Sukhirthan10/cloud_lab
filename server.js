77ef89d9d082d80447d1f3e273c91a263bd89213
Sukhirthan A <166843865+Sukhirthan10@users.noreply.github.com>
2026-04-08 18:21:06 +0530

    Initialize Express server with basic route

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('DevSecOps Pipeline App is Running!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
